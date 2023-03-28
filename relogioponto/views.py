c8fb8a59e8aadbd75776e275c1fd1e8f073ae2da
gustavobmiranda <gustavobmd3@hotmail.com>
2023-03-27 22:43:36 -0300

    ahh

--- relogioponto/views.py
+++ relogioponto/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from django.shortcuts import redirect
 from relogioponto.models import Funcionarios, Ferias, RegistroPonto, Setor, SaldoDiario, HorasExtras, Periodo
 from relogioponto.forms import FuncionariosForm, FeriasForm, RegistroPontoForm, SetorForm, SaldoDiarioForm, HorasExtrasForm, PeriodoForm
 
